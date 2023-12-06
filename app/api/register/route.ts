1a705c59e9c4aebda6905531601c18e1387903cb
srijan <srijan.dhl21@gmail.com>
2023-12-06 22:55:53 +1100

    landing pages

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,13 +1,13 @@
 
-import  bcrypt  from 'bcrypt';
+import bcrypt from 'bcrypt';
 import prisma from "@/app/lib/prismadb";
 
 import { NextResponse } from 'next/server';
-    
-export async function POST(request:Request) {
-    const body = await request.json()
-    const {email,name,password} = body
-    const hashedPassword = await bcrypt.hash(password,10)
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const { email, name, password } = body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.user.create({
         data: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-'use client'
+"use client";
+import { CommandMenu } from "@/components/commandMenu/CommandMenu";
 import { Navigation } from "@/components/navbar/Navigation";
+import { Button } from "@/components/ui/button";
 
+import Link from "next/link";
 
 export default function Home() {
-  return <Navigation/>
+  return (
+    <div className="flex justify-between items-center w-max-screen h-max-screen  h-full m-3">
+      <div className="flex items-center">
+        Logo
+        <Navigation />
+      </div>
+
+      <div className="flex gap-2">
+        <div>
+          <Button>
+            <CommandMenu />
+          </Button>
+        </div>
+        <div className="flex gap-2">
+          <Button asChild variant="outline">
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/register">Register</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
